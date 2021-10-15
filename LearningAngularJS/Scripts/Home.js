b84f8e566e07bb28f3e5f2bae538cbef1b51fc7d
Arpit S <arpittempacc@gmail.com>
2021-10-14 22:18:59 -0400

    Adding  module, controller and sample html page

---
+++ LearningAngularJS/Scripts/Home.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="angular.min.js" />
+var myModule = angular.module("myModule", []);
+
+var myController = function ($scope) {
+    var employee = {
+        firstName: 'Arpit',
+        gender: 'Male',
+        department:'IT'
+    };
+    $scope.employee = employee;
+    $scope.message = "Welcome to the AngularJS";
+};
+
+myModule.controller("myController", myController);
