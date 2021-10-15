d89911ffb40ae0f487397e1ccd2b5c28e89455f6
Arpit S <arpittempacc@gmail.com>
2021-10-14 23:13:07 -0400

    Understanding events and filters in angularJs

--- LearningAngularJS/Scripts/Home.js
+++ LearningAngularJS/Scripts/Home.js
@@ -5,10 +5,37 @@ var myController = function ($scope) {
     var employee = {
         firstName: 'Arpit',
         gender: 'Male',
-        department:'IT'
+        department: 'IT'
     };
+
+    var employees = [
+        { firstName: 'Emp1', gender: 'Male', department: 'IT',salary:55000.56 },
+        { firstName: 'Emp2', gender: 'Male', department: 'IT', salary: 65000.77 },
+        { firstName: 'Emp3', gender: 'Female', department: 'IT', salary: 85000.06 },
+        { firstName: 'Emp4', gender: 'Male', department: 'IT', salary: 55000.56 },
+        { firstName: 'Emp5', gender: 'Male', department: 'IT', salary: 65000.77 },
+        { firstName: 'Emp6', gender: 'Female', department: 'IT', salary: 85000.06 }
+    ];
+
+    var languages = [
+        { name: 'English', likes: 0, dislikes: 0 },
+        { name: 'Hindi', likes: 0, dislikes: 0 },
+        { name: 'Gujarati', likes: 0, dislikes: 0 }
+    ]
     $scope.employee = employee;
+    $scope.employees = employees;
+    $scope.languages = languages;
     $scope.message = "Welcome to the AngularJS";
+
+    $scope.incrementLikes = function (lang) {
+        lang.likes++;
+    };
+
+    $scope.incrementDislikes = function (lang) {
+        lang.dislikes++;
+    };
+
+    $scope.rowlimit = 3;
 };
 
 myModule.controller("myController", myController);
